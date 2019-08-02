fad79deadb06be294dddba05e989aba9e81dad33
nabilParis75 <nab.dahlal@gmail.com>
2019-08-02 16:05:08 +0200

    commit my site

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import DefaultLayout from '~/layouts/Default.vue'
+require('./assets/style.css')
+export default function (Vue, { head }) {
+  Vue.component('Layout', DefaultLayout)
+  head.link.push({
+    rel: 'stylesheet',
+    href: 'https://fonts.googleapis.com/css?family=Exo%7CRubik&display=swap'
+  })
+}
